553595ab79e9f00eb749210fa9db9ce34719fad5
zy16373 <zy16373@gmail.com>
2017-09-28 14:40:56 +0800

    show first image

--- lab1/MLElab1.m
+++ lab1/MLElab1.m
@@ -15,18 +15,22 @@ end
 % The next 4 bytes is num of images in total
 total = fread(fid, 1, 'int32');
 % The next 4 bytes is num of rows
+% 28 in this case
 numRows = fread(fid, 1, 'int32');
 % The next 4 bytes is num of columns
+% 28 in this case
 numCols = fread(fid, 1, 'int32');
 % Store image into an individual slice
+% Each image is 28 * 28 * 8bits(1byte)
 images = zeros(numRows, numCols, totalImages, 'uint8');
 for k = 1 : total
     % Read in numRows*numCols pixels at a time
     A = fread(fid1, numRows*numCols, 'uint8');
     % Reshape so that it becomes a matrix
-    % We are actually reading this in column major format
-    % so we need to transpose this at the end
     images(:,:,k) = reshape(uint8(A), numCols, numRows).';
 end
 % Close the file
 fclose(fid);
+% Display first digit
+figure;
+imshow(images(:,:,1),[0 255]);
